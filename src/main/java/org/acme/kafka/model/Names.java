e53de809711e0051ea68989dcf43154726ec8aeb
Marcus Paulo <marcuspaulo.melo@bbts.com.br>
2021-02-23 11:21:18 -0300

    Quarkus Kafka - InMemory test

---
+++ src/main/java/org/acme/kafka/model/Names.java
@@ -0,0 +1,46 @@
+package org.acme.kafka.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+class Names {
+
+    private static final List<String> VALUES = Arrays.asList(
+            "Olivia",
+            "Oliver",
+            "Amelia",
+            "George",
+            "Isla",
+            "Harry",
+            "Ava",
+            "Noah",
+            "Emily",
+            "Jack",
+            "Sophia",
+            "Charlie",
+            "Grace",
+            "Leo",
+            "Mia",
+            "Jacob",
+            "Poppy",
+            "Freddie",
+            "Ella",
+            "Alfie",
+            "Tom",
+            "Julie",
+            "Matt",
+            "Joe",
+            "Zoe"
+    );
+
+    static String pickAName() {
+        Random random = new Random();
+        int index = random.nextInt(VALUES.size());
+        return VALUES.get(index);
+    }
+
+    private Names() {
+        // avoid direct instantiation
+    }
+}
